1872fcb113fddfda993225a46ecfff70bb1f39a0
Incognito Svetoslav <venom008@gmail.com>
2015-05-30 13:45:31 +0500

    Initial project

---
+++ MonoDevelop.VersionControl.Mercurial/MonoDevelop.VersionControl.Mercurial/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin(
+	"MonoDevelop.VersionControl.Mercurial", 
+	Namespace = "MonoDevelop.VersionControl.Mercurial",
+	Version = "1.0"
+)]
+
+[assembly:AddinName("MonoDevelop.VersionControl.Mercurial")]
+[assembly:AddinCategory("IDE extensions")]
+[assembly:AddinDescription("MonoDevelop.VersionControl.Mercurial")]
+[assembly:AddinAuthor("Svetoslav Karasev")]
+
