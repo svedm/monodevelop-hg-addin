fa908d95f18a386a482a0d34cca31ee5213a0049
Incognito Svetoslav <venom008@gmail.com>
2015-07-01 00:54:25 +0500

    add MercurialClient - Hg.Net wrap

---
+++ MonoDevelop.VersionControl.Mercurial/MonoDevelop.VersionControl.Mercurial/MercurialClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Hg.Net;
+using System.Collections.Generic;
+
+namespace MonoDevelop.VersionControl.Mercurial
+{
+	public class MercurialClient
+	{
+		private readonly Hg.Net.HgClient _hgClient;
+
+		public MercurialClient(string repoPath, string mercurialPath)
+		{
+			_hgClient = new HgClient(mercurialPath);
+			_hgClient.Connect(repoPath);
+		}
+
+		public string Cat(string path, string revision)
+		{
+			if (string.IsNullOrEmpty(path))
+			{
+				throw new ArgumentException("Path cannot be empty");
+			}
+
+			var args = new List<string> { "cat", path };
+			if (!string.IsNullOrEmpty(revision))
+			{
+				args.Add("--rev");
+				args.Add(revision);
+			}
+			return _hgClient.ExecuteCommand(args).Response;
+		}
+	}
+}
+
