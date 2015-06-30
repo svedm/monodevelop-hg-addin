73710c039e8524902a692c0f5f8a74e96b789850
Incognito Svetoslav <venom008@gmail.com>
2015-07-01 00:55:17 +0500

    Implement Revision class

---
+++ MonoDevelop.VersionControl.Mercurial/MonoDevelop.VersionControl.Mercurial/Properties/MercurialRevision.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MonoDevelop.VersionControl.Mercurial
+{
+	public class MercurialRevision : Revision
+	{
+		public readonly string RevisionNumber;
+
+
+
+		public const string Head = "tip";
+
+		public MercurialRevision(Repository repository, string revisionNumber)
+			: base(repository)
+		{
+			if (string.IsNullOrEmpty(revisionNumber))
+			{
+				throw new ArgumentException("revision number cannot be emty");
+			}
+			this.RevisionNumber = revisionNumber;
+		}
+
+		#region implemented abstract members of Revision
+
+		public override Revision GetPrevious()
+		{
+			int revisionNumber;
+			if (int.TryParse(RevisionNumber, out revisionNumber))
+			{
+				return new MercurialRevision(this.Repository, (revisionNumber - 1).ToString());
+			}
+			else
+				return new MercurialRevision(Repository, string.Format("p1({0})", RevisionNumber));
+		}
+
+		#endregion
+	}
+}
+
