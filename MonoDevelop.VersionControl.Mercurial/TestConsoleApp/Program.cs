7c23eb740cb43ec49e5fdc98d192a17ee6c9e278
Incognito Svetoslav <venom008@gmail.com>
2015-07-02 00:07:47 +0500

    2 test project for test library on other OS

---
+++ MonoDevelop.VersionControl.Mercurial/TestConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MonoDevelop.VersionControl.Mercurial;
+
+namespace TestConsoleApp
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var md = new MercurialClient("/Users/VeNOm/Documents/testRepo", "/usr/local/bin/hg");
+
+			//var res = md.Cat("/Users/VeNOm/Documents/testRepo/qwe.txt", null);
+
+			Console.WriteLine("qwe");
+		}
+	}
+}
