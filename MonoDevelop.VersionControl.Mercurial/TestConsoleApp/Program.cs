14679dcb15f584824bf75353fb92757e3039656d
Incognito Svetoslav <venom008@gmail.com>
2015-07-07 11:15:50 +0500

    Change test project

--- MonoDevelop.VersionControl.Mercurial/TestConsoleApp/Program.cs
+++ MonoDevelop.VersionControl.Mercurial/TestConsoleApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using MonoDevelop.VersionControl.Mercurial;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace TestConsoleApp
 {
@@ -7,11 +9,49 @@ namespace TestConsoleApp
 	{
 		public static void Main(string[] args)
 		{
-			var md = new MercurialClient("/Users/VeNOm/Documents/testRepo", "/usr/local/bin/hg");
+			var currentPath = Directory.GetCurrentDirectory();
+			var testRepoPath = Path.Combine(currentPath, "testRepo");
+			if (Directory.Exists(testRepoPath))
+			{
+				Directory.Delete(testRepoPath, true);
+			}
 
-			//var res = md.Cat("/Users/VeNOm/Documents/testRepo/qwe.txt", null);
+			Directory.CreateDirectory(testRepoPath);
 
-			Console.WriteLine("qwe");
+			var os = Environment.OSVersion.VersionString.ToLower();
+			var hgPath = (os.Contains("win")) ? "hg" : (IsRunningOnMac()) ? "/usr/local/bin/hg" : "/usr/bin/hg";
+
+			MercurialClient.Init(testRepoPath, hgPath);
+
+			var mc = new MercurialClient(testRepoPath, hgPath);
+
+			var testFilePath = Path.Combine(testRepoPath, "qwe.txt");
+			File.AppendAllText(testFilePath, "qweqwe");
+
+			mc.Add(new string[] { testFilePath });
+			mc.Commit("first commit");
+		}
+
+		[DllImport ("libc")]
+		static extern int uname (IntPtr buf);
+
+		public static bool IsRunningOnMac ()
+		{
+			IntPtr buf = IntPtr.Zero;
+			try {
+				buf = Marshal.AllocHGlobal (8192);
+				// This is a hacktastic way of getting sysname from uname ()
+				if (uname (buf) == 0) {
+					string os = Marshal.PtrToStringAnsi (buf);
+					if (os == "Darwin")
+						return true;
+				}
+			} catch {
+			} finally {
+				if (buf != IntPtr.Zero)
+					Marshal.FreeHGlobal (buf);
+			}
+			return false;
 		}
 	}
 }
