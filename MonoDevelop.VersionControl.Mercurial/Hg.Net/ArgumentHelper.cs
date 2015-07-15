0b20c1fd487d5341b9b3886cfa5582d6949138ca
Svetoslav Incognito <venom008@gmail.com>
2015-07-16 00:30:31 +0500

    Adding helper for working with hg arguments

---
+++ MonoDevelop.VersionControl.Mercurial/Hg.Net/ArgumentHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Hg.Net
+{
+	public class ArgumentHelper
+	{
+		private readonly List<string> _argumentsList;
+ 
+		public ArgumentHelper ()
+		{
+			_argumentsList = new List<string>();
+		}
+
+		public void AddIf(bool condition, params string[] arguments)
+		{
+			if (condition)
+			{
+				_argumentsList.AddRange(arguments);
+			}
+		}
+
+		public void AddIfNotNullOrEmpty(params string[] arguments)
+		{
+			foreach (var argument in arguments.Where(argument => !string.IsNullOrEmpty(argument)))
+			{
+				_argumentsList.Add(argument);
+			}
+		}
+	}
+}
+
