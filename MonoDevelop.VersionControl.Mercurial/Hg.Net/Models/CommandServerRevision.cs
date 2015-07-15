8e179d41b2fb90c00c15eb21f540d314b21f8444
Svetoslav Incognito <venom008@gmail.com>
2015-07-16 02:46:18 +0500

    Add CommandServerRevision model & XmlHelper for parsing hg log output

---
+++ MonoDevelop.VersionControl.Mercurial/Hg.Net/Models/CommandServerRevision.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Xml;
+
+namespace Hg.Net
+{
+	public class CommandServerRevision
+	{
+		public string RevisionId { get; private set; }
+
+		public DateTime Date { get; private set; }
+
+		public string Author { get; private set; }
+
+		public string Email { get; private set; }
+
+		public string Message { get; private set; }
+
+		public CommandServerRevision(string revision, DateTime date, string author, string email, string message)
+		{
+			RevisionId = revision;
+			Date = date;
+			Author = author;
+			Message = message;
+			Email = email;
+		}
+
+		public CommandServerRevision(XmlNode node)
+		{
+			if (node.Attributes != null)
+			{
+				RevisionId = node.Attributes["revision"].Value;
+
+				var date = node.SelectSingleNode("date");
+				if (date != null)
+				{
+					Date = DateTime.Parse(date.InnerText);
+				}
+
+				var author = node.SelectSingleNode("author");
+				if (author != null)
+				{
+					Email = author.InnerText;
+					if (author.Attributes != null)
+					{
+						Author = author.Attributes["email"].Value;
+					}
+				}
+			}
+			var selectSingleNode = node.SelectSingleNode("msg");
+			if (selectSingleNode != null)
+			{
+				Message = selectSingleNode.InnerText;
+			}
+		}
+	}
+}
+
---
+++ MonoDevelop.VersionControl.Mercurial/Hg.Net/XmlHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Linq;
+
+namespace Hg.Net
+{
+	public class XmlHelper
+	{
+		public static IList<CommandServerRevision> GetRevisions(string text)
+		{
+			var document = new XmlDocument ();
+			document.LoadXml (text);
+
+			var revisions = new List<CommandServerRevision> ();
+			var xmlNodeList = document.SelectNodes("/log/logentry");
+			if (xmlNodeList != null)
+			{
+				revisions.AddRange(xmlNodeList.Cast<XmlNode>().Select(n => new CommandServerRevision(n)));
+			}
+
+			return revisions;
+		}
+	}
+}
+
