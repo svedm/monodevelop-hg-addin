a7137d573acaa0bd44881d6d79cc55d2bf67bd7c
Svetoslav Incognito <venom008@gmail.com>
2015-07-28 23:42:38 +0500

    VS code style

--- MonoDevelop.VersionControl.Mercurial/Hg.Net/ArgumentHelper.cs
+++ MonoDevelop.VersionControl.Mercurial/Hg.Net/ArgumentHelper.cs
@@ -5,49 +5,49 @@ using Hg.Net.Models;
 
 namespace Hg.Net
 {
-	public class ArgumentHelper
-	{
-		private readonly List<string> _argumentsList;
- 
-		public ArgumentHelper ()
-		{
-			_argumentsList = new List<string>();
-		}
+    public class ArgumentHelper
+    {
+        private readonly List<string> _argumentsList;
 
-		public void AddIf(bool condition, params string[] arguments)
-		{
-			if (condition)
-			{
-				_argumentsList.AddRange(arguments);
-			}
-		}
+        public ArgumentHelper()
+        {
+            _argumentsList = new List<string>();
+        }
 
-		public void AddIfNotNullOrEmpty(bool throwExceptonIfFalse, params string[] arguments)
-		{
-			if (arguments.Any(string.IsNullOrEmpty))
-			{
-				if (throwExceptonIfFalse)
-				{
-					throw new ArgumentException("can not be bull or empty", arguments.First(string.IsNullOrEmpty));
-				}
-				return;
-			}
-			_argumentsList.AddRange(arguments);
-		}
+        public void AddIf(bool condition, params string[] arguments)
+        {
+            if (condition)
+            {
+                _argumentsList.AddRange(arguments);
+            }
+        }
 
-		public void Add(params string[] argumets)
-		{
-			_argumentsList.AddRange(argumets);
-		}
+        public void AddIfNotNullOrEmpty(bool throwExceptonIfFalse, params string[] arguments)
+        {
+            if (arguments.Any(string.IsNullOrEmpty))
+            {
+                if (throwExceptonIfFalse)
+                {
+                    throw new ArgumentException("can not be bull or empty", arguments.First(string.IsNullOrEmpty));
+                }
+                return;
+            }
+            _argumentsList.AddRange(arguments);
+        }
 
-		public List<string> GetList()
-		{
-			return _argumentsList;
-		}
+        public void Add(params string[] argumets)
+        {
+            _argumentsList.AddRange(argumets);
+        }
 
-		public override string ToString()
-		{
-			return string.Join(" ", _argumentsList);
+        public List<string> GetList()
+        {
+            return _argumentsList;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(" ", _argumentsList);
 		}
 
 		public static string ArgumentForStatus (Status status)
@@ -72,7 +72,7 @@ namespace Hg.Net
 				default:
 					return string.Empty;
 			}
-		}
-	}
+        }
+    }
 }
 
--- MonoDevelop.VersionControl.Mercurial/Hg.Net/HgCommandServerClient.cs
+++ MonoDevelop.VersionControl.Mercurial/Hg.Net/HgCommandServerClient.cs
@@ -10,235 +10,235 @@ using Hg.Net.Models;
 
 namespace Hg.Net
 {
-	public class HgCommandServerClient
-	{
-		public List<string> Capabilities { get; private set; }
-		public string HgEncoding { get; private set; }
-		private readonly string _hgPath = "hg";
-		private const byte HeaderLength = 5;
-		private Process _cmdServer;
+    public class HgCommandServerClient
+    {
+        public List<string> Capabilities { get; private set; }
+        public string HgEncoding { get; private set; }
+        private readonly string _hgPath = "hg";
+        private const byte HeaderLength = 5;
+        private Process _cmdServer;
 
-		public HgCommandServerClient()
-		{
+        public HgCommandServerClient()
+        {
 
-		}
+        }
 
-		public HgCommandServerClient(string hgPath)
-			: this()
-		{
-			_hgPath = hgPath;
-		}
+        public HgCommandServerClient(string hgPath)
+            : this()
+        {
+            _hgPath = hgPath;
+        }
 
-		public bool Connect(string pathToRepo)
-		{
-			var args = string.Format(@"serve --cmdserver pipe --cwd ""{0}"" --repository ""{0}""", pathToRepo);
-			var serverInfo = new ProcessStartInfo(_hgPath, args)
-			{
-				RedirectStandardInput = true,
-				RedirectStandardError = true,
-				RedirectStandardOutput = true,
-				CreateNoWindow = true,
-				UseShellExecute = false
-			};
-			serverInfo.EnvironmentVariables.Add("HGENCODING", "UTF-8");
+        public bool Connect(string pathToRepo)
+        {
+            var args = string.Format(@"serve --cmdserver pipe --cwd ""{0}"" --repository ""{0}""", pathToRepo);
+            var serverInfo = new ProcessStartInfo(_hgPath, args)
+            {
+                RedirectStandardInput = true,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+            serverInfo.EnvironmentVariables.Add("HGENCODING", "UTF-8");
 
-			try
-			{
-				_cmdServer = Process.Start(serverInfo);
-				Hello();
-			}
-			catch (Exception)
-			{
-				return false;
-			}
+            try
+            {
+                _cmdServer = Process.Start(serverInfo);
+                Hello();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
 
-			return true;
-		}
+            return true;
+        }
 
-		public static void Init(string path, string hgPath)
-		{
-			if (string.IsNullOrEmpty(path))
-			{
-				throw new ArgumentException("Path cannot be empty");
-			}
+        public static void Init(string path, string hgPath)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentException("Path cannot be empty");
+            }
 
-			if (string.IsNullOrEmpty(hgPath))
-			{
-				throw new ArgumentException("Mercurial path cannot be empty");
-			}
+            if (string.IsNullOrEmpty(hgPath))
+            {
+                throw new ArgumentException("Mercurial path cannot be empty");
+            }
 
-			var args = string.Format("init {0}", path);
+            var args = string.Format("init {0}", path);
 
-			var processInfo = new ProcessStartInfo(hgPath, args)
-				{
-					RedirectStandardInput = true,
-					RedirectStandardError = true,
-					RedirectStandardOutput = true,
-					CreateNoWindow = true,
-					UseShellExecute = false
-				};
-			processInfo.EnvironmentVariables.Add("HGENCODING", "UTF-8");
+            var processInfo = new ProcessStartInfo(hgPath, args)
+            {
+                RedirectStandardInput = true,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+                UseShellExecute = false
+            };
+            processInfo.EnvironmentVariables.Add("HGENCODING", "UTF-8");
 
-			var process = Process.Start(processInfo);
+            var process = Process.Start(processInfo);
 
-		    if (process == null) throw new Exception("Failed to start process");
+            if (process == null) throw new Exception("Failed to start process");
 
-		    process.WaitForExit();
+            process.WaitForExit();
 
-		    if (process.ExitCode != 0 || !string.IsNullOrEmpty(process.StandardError.ReadToEnd()))
-		    {
-		        throw new Exception("Init reposiory failed on path " + path);
-		    }
-		}
+            if (process.ExitCode != 0 || !string.IsNullOrEmpty(process.StandardError.ReadToEnd()))
+            {
+                throw new Exception("Init reposiory failed on path " + path);
+            }
+        }
 
-		private void Hello()
-		{
-			var response = ReadResponse();
+        private void Hello()
+        {
+            var response = ReadResponse();
 
-			if (response.Channel != Channel.O || !response.Messsage.Contains("capabilities") || !response.Messsage.Contains("encoding"))
-			{
-				throw new ServerException("Handshake failed");
-			}
+            if (response.Channel != Channel.O || !response.Messsage.Contains("capabilities") || !response.Messsage.Contains("encoding"))
+            {
+                throw new ServerException("Handshake failed");
+            }
 
-			var parsedMessage = response.Messsage.Split('\n')
-				.Select(s => s.Split(new[] { ": " }, StringSplitOptions.RemoveEmptyEntries)).ToDictionary(t => t[0], t => t[1]);
+            var parsedMessage = response.Messsage.Split('\n')
+                .Select(s => s.Split(new[] { ": " }, StringSplitOptions.RemoveEmptyEntries)).ToDictionary(t => t[0], t => t[1]);
 
-			Capabilities = parsedMessage["capabilities"].Split(' ').ToList();
-			HgEncoding = parsedMessage["encoding"];
-		}
+            Capabilities = parsedMessage["capabilities"].Split(' ').ToList();
+            HgEncoding = parsedMessage["encoding"];
+        }
 
-		private static int ReadBytes(Stream stream, byte[] buffer, int offset, int length)
-		{
-			var remaining = length;
-			var read = 1;
+        private static int ReadBytes(Stream stream, byte[] buffer, int offset, int length)
+        {
+            var remaining = length;
+            var read = 1;
 
-			while (remaining > 0 && read > 0)
-			{
-				read = stream.Read(buffer, offset, remaining);
-				offset += read;
-				remaining -= read;
-			}
+            while (remaining > 0 && read > 0)
+            {
+                read = stream.Read(buffer, offset, remaining);
+                offset += read;
+                remaining -= read;
+            }
 
-			return length - remaining;
-		}
+            return length - remaining;
+        }
 
-		private int RunCommand(IEnumerable<string> command, IDictionary<Channel, Stream> outputs)
-		{
-			var commandBuffer = Encoding.UTF8.GetBytes("runcommand\n");
-			var enumerable = command as string[] ?? command.ToArray();
+        private int RunCommand(IEnumerable<string> command, IDictionary<Channel, Stream> outputs)
+        {
+            var commandBuffer = Encoding.UTF8.GetBytes("runcommand\n");
+            var enumerable = command as string[] ?? command.ToArray();
 
-			var argBuffer = enumerable.Aggregate(new List<byte>(), (b, a) =>
-			{
-				b.AddRange(Encoding.UTF8.GetBytes(a));
-				b.Add(0);
-				return b;
-			}, b =>
-			{
-				b.RemoveAt(b.Count - 1);
-				return b.ToArray();
-			}).ToArray();
+            var argBuffer = enumerable.Aggregate(new List<byte>(), (b, a) =>
+            {
+                b.AddRange(Encoding.UTF8.GetBytes(a));
+                b.Add(0);
+                return b;
+            }, b =>
+            {
+                b.RemoveAt(b.Count - 1);
+                return b.ToArray();
+            }).ToArray();
 
-			var lenBuffer = BitConverter.GetBytes(argBuffer.Length);
-			if (BitConverter.IsLittleEndian)
-			{
-				Array.Reverse(lenBuffer);
-			}
+            var lenBuffer = BitConverter.GetBytes(argBuffer.Length);
+            if (BitConverter.IsLittleEndian)
+            {
+                Array.Reverse(lenBuffer);
+            }
 
-			lock (_cmdServer)
-			{
-				_cmdServer.StandardInput.BaseStream.Write(commandBuffer, 0, commandBuffer.Length); //TODO: wtf? why it returns "abort: unknown command ﻿runcommand" on mac or linux
-				_cmdServer.StandardInput.BaseStream.Write(lenBuffer, 0, lenBuffer.Length);
-				_cmdServer.StandardInput.BaseStream.Write(argBuffer, 0, argBuffer.Length);
-				_cmdServer.StandardInput.BaseStream.Flush();
+            lock (_cmdServer)
+            {
+                _cmdServer.StandardInput.BaseStream.Write(commandBuffer, 0, commandBuffer.Length); //TODO: wtf? why it returns "abort: unknown command ﻿runcommand" on mac or linux
+                _cmdServer.StandardInput.BaseStream.Write(lenBuffer, 0, lenBuffer.Length);
+                _cmdServer.StandardInput.BaseStream.Write(argBuffer, 0, argBuffer.Length);
+                _cmdServer.StandardInput.BaseStream.Flush();
 
-				try
-				{
-					while (true)
-					{
-						var response = ReadResponse();
-						if (response.Channel == Channel.R)
-							return GetMessageLength(response.Buffer, 0);
+                try
+                {
+                    while (true)
+                    {
+                        var response = ReadResponse();
+                        if (response.Channel == Channel.R)
+                            return GetMessageLength(response.Buffer, 0);
 
-						if (outputs != null && outputs.ContainsKey(response.Channel))
-						{
-							outputs[response.Channel].Write(response.Buffer, 0, response.Buffer.Length);
-						}
-					}
-				}
-				catch (Exception ex)
-				{
-					Console.WriteLine(string.Join(" ", enumerable.ToArray()));
-					Console.WriteLine(ex);
-					_cmdServer.StandardOutput.BaseStream.Flush();
-					_cmdServer.StandardError.BaseStream.Flush();
-					throw;
-				}
-			}
-		}
+                        if (outputs != null && outputs.ContainsKey(response.Channel))
+                        {
+                            outputs[response.Channel].Write(response.Buffer, 0, response.Buffer.Length);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(string.Join(" ", enumerable.ToArray()));
+                    Console.WriteLine(ex);
+                    _cmdServer.StandardOutput.BaseStream.Flush();
+                    _cmdServer.StandardError.BaseStream.Flush();
+                    throw;
+                }
+            }
+        }
 
-		private ServerResponse ReadResponse()
-		{
-			var header = new byte[HeaderLength];
-			int bytesRead;
+        private ServerResponse ReadResponse()
+        {
+            var header = new byte[HeaderLength];
+            int bytesRead;
 
-			try
-			{
-				bytesRead = ReadBytes(_cmdServer.StandardOutput.BaseStream, header, 0, HeaderLength);
-			}
-			catch (Exception ex)
-			{
-				throw new ServerException("Error when try to read command from server", ex);
-			}
+            try
+            {
+                bytesRead = ReadBytes(_cmdServer.StandardOutput.BaseStream, header, 0, HeaderLength);
+            }
+            catch (Exception ex)
+            {
+                throw new ServerException("Error when try to read command from server", ex);
+            }
 
-			if (bytesRead != HeaderLength)
-			{
-				throw new ServerException(string.Format("Invalid response header length of {0} bytes", bytesRead));
-			}
+            if (bytesRead != HeaderLength)
+            {
+                throw new ServerException(string.Format("Invalid response header length of {0} bytes", bytesRead));
+            }
 
-			var channel = (Channel)header[0];
-			var messageLength = GetMessageLength(header, 1);
+            var channel = (Channel)header[0];
+            var messageLength = GetMessageLength(header, 1);
 
-			if (channel == Channel.I || channel == Channel.L)
-				return new ServerResponse(channel, messageLength.ToString());
+            if (channel == Channel.I || channel == Channel.L)
+                return new ServerResponse(channel, messageLength.ToString());
 
-			var messageBuffer = new byte[messageLength];
+            var messageBuffer = new byte[messageLength];
 
-			try
-			{
-				bytesRead = ReadBytes(_cmdServer.StandardOutput.BaseStream, messageBuffer, 0, messageLength);
-			}
-			catch (Exception ex)
-			{
-				throw new ServerException("Error when try to read command from server", ex);
-			}
+            try
+            {
+                bytesRead = ReadBytes(_cmdServer.StandardOutput.BaseStream, messageBuffer, 0, messageLength);
+            }
+            catch (Exception ex)
+            {
+                throw new ServerException("Error when try to read command from server", ex);
+            }
 
-			if (bytesRead != messageLength)
-			{
-				throw new ServerException(string.Format("Error when try to read command from server: Expected {0} bytes, read {1}", messageLength, bytesRead));
-			}
+            if (bytesRead != messageLength)
+            {
+                throw new ServerException(string.Format("Error when try to read command from server: Expected {0} bytes, read {1}", messageLength, bytesRead));
+            }
 
-			var message = new ServerResponse((Channel)header[0], messageBuffer);
-			return message;
-		}
+            var message = new ServerResponse((Channel)header[0], messageBuffer);
+            return message;
+        }
 
-		public CommandResponse ExecuteCommand(IEnumerable<string> command)
-		{
-			var output = new MemoryStream();
-			var error = new MemoryStream();
-			var outputs = new Dictionary<Channel, Stream>() {
-				{ Channel.O, output },
-				{ Channel.E, error },
-			};
+        public CommandResponse ExecuteCommand(IEnumerable<string> command)
+        {
+            var output = new MemoryStream();
+            var error = new MemoryStream();
+            var outputs = new Dictionary<Channel, Stream>() {
+                { Channel.O, output },
+                { Channel.E, error },
+            };
 
-			var result = RunCommand(command, outputs);
-			return new CommandResponse(result, Encoding.UTF8.GetString(output.GetBuffer(), 0, (int)output.Length), 
-				Encoding.UTF8.GetString(error.GetBuffer(), 0, (int)error.Length));
-		}
+            var result = RunCommand(command, outputs);
+            return new CommandResponse(result, Encoding.UTF8.GetString(output.GetBuffer(), 0, (int)output.Length),
+                Encoding.UTF8.GetString(error.GetBuffer(), 0, (int)error.Length));
+        }
 
-		private static int GetMessageLength(byte[] buffer, int offset)
-		{
-			return IPAddress.NetworkToHostOrder(BitConverter.ToInt32(buffer, offset));
-		}
-	}
+        private static int GetMessageLength(byte[] buffer, int offset)
+        {
+            return IPAddress.NetworkToHostOrder(BitConverter.ToInt32(buffer, offset));
+        }
+    }
 }
 
