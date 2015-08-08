180bc689eb9f5c5108042eabfcd359c95f59bccb
Incognito Svetoslav <venom008@gmail.com>
2015-08-08 20:42:28 +0500

    Some adding settings

--- MonoDevelop.VersionControl.Mercurial/MonoDevelop.VersionControl.Mercurial/Properties/AddinInfo.cs
+++ MonoDevelop.VersionControl.Mercurial/MonoDevelop.VersionControl.Mercurial/Properties/AddinInfo.cs
@@ -13,3 +13,6 @@ using Mono.Addins.Description;
 [assembly:AddinDescription("MonoDevelop.VersionControl.Mercurial")]
 [assembly:AddinAuthor("Svetoslav Karasev")]
 
+[assembly:AddinDependency("Core", "5.9")]
+[assembly:AddinDependency("Ide", "5.9")]
+[assembly:AddinDependency("VersionControl", "5.9")]
