7fd8da6767eae6362b250965430ac1d5cc7154a1
Incognito Svetoslav <venom008@gmail.com>
2015-08-12 17:16:21 +0500

    Fix build

--- MonoDevelop.VersionControl.Mercurial/MonoDevelop.VersionControl.Mercurial/GUI/PasswordDialog.cs
+++ MonoDevelop.VersionControl.Mercurial/MonoDevelop.VersionControl.Mercurial/GUI/PasswordDialog.cs
@@ -7,7 +7,7 @@ namespace MonoDevelop.VersionControl.Mercurial.GUI
 		public PasswordDialog(string prompt)
 		{
 			this.Build();
-			this.Parent = MonoDevelop.Ide.IdeApp.Workbench.RootWindow
+			this.Parent = MonoDevelop.Ide.IdeApp.Workbench.RootWindow;
 			this.promptLabel.Text = GLib.Markup.EscapeText(prompt);
 		}
 	}
