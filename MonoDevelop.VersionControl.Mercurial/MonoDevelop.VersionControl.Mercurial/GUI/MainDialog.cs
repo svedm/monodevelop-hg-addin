5dcf83ebf8aa4a13fd83e4c82027fed147365391
Incognito Svetoslav <venom008@gmail.com>
2015-08-12 16:34:26 +0500

    Add main & password dialogs

---
+++ MonoDevelop.VersionControl.Mercurial/MonoDevelop.VersionControl.Mercurial/GUI/MainDialog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MonoDevelop.VersionControl.Mercurial.GUI
+{
+	public partial class MainDialog : Gtk.Dialog
+	{
+		public MainDialog()
+		{
+			this.Build();
+		}
+	}
+}
+
