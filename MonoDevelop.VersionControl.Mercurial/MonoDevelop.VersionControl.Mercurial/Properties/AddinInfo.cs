188426df34954c1b29a260df5f2d595ab9e133a6
Incognito Svetoslav <venom008@gmail.com>
2015-08-17 00:22:38 +0500

    Change adding files

--- MonoDevelop.VersionControl.Mercurial/MonoDevelop.VersionControl.Mercurial/Properties/AddinInfo.cs
+++ MonoDevelop.VersionControl.Mercurial/MonoDevelop.VersionControl.Mercurial/Properties/AddinInfo.cs
@@ -2,18 +2,15 @@
 using Mono.Addins;
 using Mono.Addins.Description;
 
-[assembly:Addin(
-	"VersionControl.Mercurial", 
+[assembly:Addin ("VersionControl.Mercurial", 
 	Namespace = "MonoDevelop",
-	Version = "1.0"
-)]
+	Version = "1.0",
+	Category = "Version Control")]
 
-[assembly:AddinName("Mercurial support")]
-[assembly:AddinCategory("Version Control")]
-[assembly:AddinDescription("MonoDevelop.VersionControl.Mercurial")]
-[assembly:AddinAuthor("Svetoslav Karasev")]
-[assembly:AddinDescription("Provides support for the Mercurial version control system")]
+[assembly:AddinName ("Mercurial support")]
+[assembly:AddinDescription ("Mercurial support for the Version Control Add-in")]
 [assembly:AddinUrl("https://github.com/svedm/monodevelop-hg-addin")]
+[assembly:AddinAuthor("Svetoslav Karasev")]
 
 [assembly:AddinDependency ("Core", MonoDevelop.BuildInfo.Version)]
 [assembly:AddinDependency ("Ide", MonoDevelop.BuildInfo.Version)]
--- MonoDevelop.VersionControl.Mercurial/MonoDevelop.VersionControl.Mercurial/Properties/AssemblyInfo.cs
+++ MonoDevelop.VersionControl.Mercurial/MonoDevelop.VersionControl.Mercurial/Properties/AssemblyInfo.cs
@@ -4,24 +4,10 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("MonoDevelop.VersionControl.Mercurial")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Svetoslav Karasev")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyProduct ("MonoDevelop")]
+[assembly: AssemblyTitle ("Mercurial support for the Version Control Add-in")]
+[assembly: AssemblyDescription ("Mercurial support for the Version Control Add-in")]
+[assembly: AssemblyVersion ("1.0")]
+[assembly: AssemblyCopyright ("(c) 2015 Karasev Svetoslav")]
 
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
+[assembly:InternalsVisibleTo ("MonoDevelop.VersionControl.Tests")]
